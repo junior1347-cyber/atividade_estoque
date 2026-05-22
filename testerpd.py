946bb14d658e85cefafeb4777578d149cae3ea8e
Jubiraci Silva S. Filho <jubiracisilva1@gmail.com>
2026-05-22 17:20:16 -0400

    construção do zero.

---
+++ testerpd.py
@@ -0,0 +1,42 @@
+import os
+from dataclasses import dataclass
+
+
+def limpar_tela():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+@dataclass
+class E_P:
+    nome: str
+    preco: float
+    quantidade: int
+
+estoque_prod = [
+	 E_P("Tablet", 5000.00, 100),
+	 E_P("Notebook", 7000.00, 100),
+	 E_P("Smartphone", 5000.00, 100),
+]
+
+while True:
+	try:
+		limpar_tela()
+
+		print("\n --- MENU PRINCIPAL ---")
+		print("1-Visulaizar estoque atual.")
+		print("2-Registrar nova entrada.")
+		print("3-Registrar nova saída.")
+		print("4-cancelar\n")
+		opcao = int(input("\nDigite uma opção válida: "))
+
+		#OPÇÃO1
+		if opcao == 1:
+			for produto in estoque_prod:
+				print(f"Produto: {produto.nome} | Valor: R$ {produto.preco:.2f} | Estoque: {produto.quantidade} Unidades")
+		#OPÇÃO2
+		if opcao == 2:
+			for produto in estoque_prod:
+				
+
+		break
+	except ValueError:
+		print()
