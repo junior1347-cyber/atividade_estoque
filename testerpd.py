c98bd24f1611a137d2cce6129a67c796cb6e3ebe
junior1347-cyber <jubiracisilva182@gmail.com>
2026-05-25 00:43:24 -0400

    em processo

--- testerpd.py
+++ testerpd.py
@@ -1,15 +1,10 @@
-import os
 from dataclasses import dataclass
-
-
-def limpar_tela():
-	os.system('cls' if os.name == 'nt' else 'clear')
 
 @dataclass
 class E_P:
     nome: str
     preco: float
-    quantidade: int
+    quantidade: int = 0
 
 estoque_prod = [
 	 E_P("Tablet", 5000.00, 100),
@@ -24,8 +19,11 @@ while True:
 		print("2-Registrar nova entrada.")
 		print("3-Registrar nova saída.")
 		print("4-cancelar\n")
-		opcao = int(input("\nDigite uma opção válida: "))
-
+		opcao = int(input("\nDigite uma opção válida: \n"))
+		if opcao < 1:
+			print("Digite apenas números de 1 a 4!")
+		if opcao > 4:
+			print("Digite apenas números de 1 a 4!")
 		#OPÇÃO1
 		if opcao == 1:
 			for produto in estoque_prod:
@@ -33,15 +31,27 @@ while True:
 		#OPÇÃO2
 		elif opcao == 2:
 			nome = input("Nome do produto: ")
-			preco = int(input("Valor: "))
-			quantidade = float(input("Quantidade: "))
+			preco = float(input("Valor: "))
+			quantidade = int(input("Quantidade: "))
 
 			novo_produto = E_P(nome, float(preco), int(quantidade))
 			estoque_prod.append(novo_produto)
 			print(f"produto {novo_produto.nome} adicionado com sucesso!")
-
+		#OPÇÃO3
+		elif opcao == 3:
+			nome = input("Nome do produto: ")
+			quantidade = int(input("Quantidade: "))
+			for produto in estoque_prod:
+				if produto.nome == nome:
+					produto.quantidade -= quantidade
+					print(f"\n{quantidade} unidades removidas do produto: {nome}")
+					break
+				elif produto.quantidade > quantidade:
+					print("Quantidade disponivel é insuficiente!")
+			else:
+				print(f"{nome} não existe!")
 		#OPÇÃO4
 		elif opcao == 4:
 			break
 	except ValueError:
-		print()
+		print("\nDigite apena números!")
